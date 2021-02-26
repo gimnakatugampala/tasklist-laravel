0161fcbf4c6193cc0980e0ad80bc00db703013c6
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-02-26 11:33:17 +0530

    complied sass

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .js('resources/js/bootstrap.js', 'public/js')
+    .sass('resources/sass/main.scss','public/css')
+    .sass('resources/sass/app.scss','public/css');
